221cfd06b55354ff93cf9cc1fd164fd97803747c
PRAJJVAL JAISWAL <prajjaiswal158@gmail.com>
2025-02-20 01:09:47 +0530

    -db

--- database/mongodb.js
+++ database/mongodb.js
@@ -9,35 +9,35 @@ const MONGODB_URI = process.env.MONGODB_URI;
 let cached = global.mongoose || { conn: null, promise: null };
 
 
-// const dbconnect= async()=>{
-//   try {
-//     await mongoose.connect("mongodb+srv://prajjaiswal158:9967138778@compusconnect.xo5zf.mongodb.net")
-//     console.log("cn")
-//   } catch (error) {
-//     console.log(error)
-//   }
-// }
-
-async function dbconnect() {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000, // Adjust timeout
-      socketTimeoutMS: 45000, // Prevents Vercel 504 errors
-    }).then((conn) => {
-      console.log("MongoDB connected!");
-      return conn;
-    }).catch((err) => {
-      console.error("MongoDB connection error:", err);
-      throw err;
-    });
+const dbconnect= async()=>{
+  try {
+    await mongoose.connect("mongodb+srv://prajjaiswal158:9967138778@compusconnect.xo5zf.mongodb.net/test")
+    console.log("cn")
+  } catch (error) {
+    console.log(error)
   }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
 }
+
+// async function dbconnect() {
+//   if (cached.conn) return cached.conn;
+
+//   if (!cached.promise) {
+//     cached.promise = mongoose.connect(MONGODB_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       serverSelectionTimeoutMS: 5000, // Adjust timeout
+//       socketTimeoutMS: 45000, // Prevents Vercel 504 errors
+//     }).then((conn) => {
+//       console.log("MongoDB connected!");
+//       return conn;
+//     }).catch((err) => {
+//       console.error("MongoDB connection error:", err);
+//       throw err;
+//     });
+//   }
+
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
 
 module.exports = dbconnect
